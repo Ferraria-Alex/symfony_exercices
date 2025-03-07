aeecb98280529df1a60e1957f6164ffa574219bd
Ferraria-Alex <alexmferrari.a@gmail.com>
2025-03-07 11:04:29 +0100

    Exercises from 07/03 - Api Symfony

--- src/Controller/ApiAccountController.php
+++ src/Controller/ApiAccountController.php
@@ -5,22 +5,30 @@ namespace App\Controller;
 use App\Entity\Account;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Repository\AccountRepository;
+use App\Repository\ArticleRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\DocBlock\Serializer;
-use Symfony\Component\HttpFoundation\Response;
+#use phpDocumentor\Reflection\DocBlock\Serializer;
+#use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Encoder\DecoderInterface;
-use Symfony\Component\Serializer\Encoder\EncoderInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+#use Symfony\Component\Serializer\Encoder\DecoderInterface;
+#use Symfony\Component\Serializer\Encoder\EncoderInterface;
+#use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+#use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 
 
 class ApiAccountController extends AbstractController{
 
-    public function __construct(private readonly AccountRepository $accountRepository, private readonly EntityManagerInterface $em, private readonly SerializerInterface $serializer){}
+    public function __construct(
+        private readonly AccountRepository $accountRepository, 
+        private readonly ArticleRepository $articleRepository,
+        private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly EntityManagerInterface $em, 
+        private readonly SerializerInterface $serializer
+        ){}
 
     #[Route('/api/accounts', name: 'api_account_all')]
     public function getAllAccounts(){
@@ -31,7 +39,7 @@ class ApiAccountController extends AbstractController{
         );
     }
 
-    #[Route('/api/accounts/add', name: 'app_account_add', methods: ['POST'])]
+    #[Route('/api/accounts/add', name: 'api_account_add', methods: ['POST'])]
     public function addAccount(Request $request){
         $request = $request->getContent();
         $account = $this->serializer->deserialize($request, Account::class, 'json');
@@ -48,7 +56,115 @@ class ApiAccountController extends AbstractController{
         return $this->json($account, $code, [
             "Access-Control-Allow-Origin" => "*",
             "Content-Type" => "application/json"
-        ], []);
+        ], ["groups" => "account:read"]);
+    }
 
+    #[Route('/api/accounts/update/{email}', name: 'api_account_update', methods: ['PUT'])]
+    public function updateAccount(Request $request, String $email){
+
+        $request = $request->getContent();
+        $updateInfo = $this->serializer->deserialize($request, Account::class, 'json');
+
+        if ($updateInfo->getFirstname() && $updateInfo->getLastname() && $email) {
+
+            $account = $this->accountRepository->findOneBy(['email' => $email]);
+
+            if($account){
+                $account->setFirstname($updateInfo->getFirstname());
+                $account->setLastname($updateInfo->getLastname());
+                $this->em->flush();
+                $code = 200;
+            } else {
+                $account = "La compte n'existe pas";
+                $code = 400;
+            }
+        } else {
+            $account = "Champs non remplis";
+            $code = 400;
+        }
+
+        return $this->json(
+            $account,
+            $code,
+            [
+                "Access-Control-Allow-Origin" => "*",
+                "Content-Type" => "application/json"
+            ],
+            ['groups' => 'account:read']
+        );
+    }
+
+    #[Route('/api/accounts/delete/{id}', name: 'api_account_delete', methods: ['DELETE'])]
+    public function deleteAccount(int $id){
+
+        $account = $this->accountRepository->findOneBy(['id' => $id]);
+        $articles = $this->articleRepository->findBy(['author' => $account]);
+
+        if(!$articles){
+            if($account){
+                $this->em->remove($account);
+                $this->em->flush();
+                $account = "La compte a ete eliminé avec success";
+                $code = 200;
+            } else {
+                $account = "La compte n'existe pas";
+                $code = 400;
+            }
+        } else {
+            $account = "La compte ne peux pas etre eliminé. Elle contient des articles associes!";
+            $code = 409;
+        }
+        return $this->json(
+            $account,
+            $code,
+            [
+                "Access-Control-Allow-Origin" => "*",
+                "Content-Type" => "application/json"
+            ],
+            ['groups' => 'account:read']
+        );
+    }
+
+    #[Route('/api/accounts/patch/{email}', name: 'api_account_patch', methods: ['PATCH'])]
+    public function patchAccount(Request $request, String $email){
+
+        $request = $request->getContent();
+        $updateInfo = $this->serializer->deserialize($request, Account::class, 'json');
+
+        if ($updateInfo->getPassword() && $email) {
+            if($updateInfo->getPassword() > 8){
+
+                $account = $this->accountRepository->findOneBy(['email' => $email]);
+
+                if($account){
+
+                    $hashedPassword = password_hash($updateInfo->getPassword(), PASSWORD_BCRYPT);
+
+                    $account->setPassword($hashedPassword);
+                    $this->em->flush();
+                    $account = "The password has been succesfully updated";
+                    $code = 200;
+                } else {
+                    $account = "La compte n'existe pas";
+                    $code = 400;
+                }
+            } else {
+                $account = "Weak Password";
+                $code = 400;
+            }
+        } else {
+            $account = "Champs non remplis";
+            $code = 400;
+        }
+
+        return $this->json(
+            $account,
+            $code,
+            [
+                "Access-Control-Allow-Origin" => "*",
+                "Content-Type" => "application/json"
+            ],
+            ['groups' => 'account:read']
+        );
     }
 }
--- src/Controller/ApiArticleController.php
+++ src/Controller/ApiArticleController.php
@@ -2,14 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\AccountRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+#use phpDocumentor\Reflection\DocBlock\Serializer;
+#use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+#use Symfony\Component\Serializer\Encoder\DecoderInterface;
+#use Symfony\Component\Serializer\Encoder\EncoderInterface;
+#use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+#use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 
 class ApiArticleController extends AbstractController{
 
-    public function __construct(Private readonly ArticleRepository $articleRepository){}
+    public function __construct(Private readonly ArticleRepository $articleRepository,
+    private readonly AccountRepository $accountRepository,
+    private readonly CategoryRepository $categoryRepository,
+    private readonly EntityManagerInterface $em,
+    private readonly SerializerInterface $serializer){}
 
     #[Route('/api/articles', name: 'api_article_all')]
     public function getAllArticles(){
@@ -20,12 +36,54 @@ class ApiArticleController extends AbstractController{
         );
     }
 
-    #[Route('/api/articles/{id}', name: 'api_article_byId')]
+    #[Route('/api/articles/{id}', name: 'api_article_byid')]
     public function getArticleById(int $id){
         return $this->json($this->articleRepository->find($id),
         200,
         [],
         ['groups' => 'articlebyid:read']
         );
+    }
+
+    #[Route('/api/articles-add', name: 'app_articles_add', methods: ['POST'])]
+    public function addArticle(Request $request){
+        $request = $request->getContent();
+        $article = $this->serializer->deserialize($request, Article::class, 'json');
+
+        if ($article->getTitle() && $article->getContent() && $article->getAuthor()) {
+
+            $article->setAuthor($this->accountRepository->findOneBy(["email" => $article->getAuthor()->getEmail()]));
+
+            foreach ($article->getCategories() as $key => $value) {
+                $cat = $value->getName();
+                $article->removeCategory($value);
+                $cat = $this->categoryRepository->findOneBy(["name" => $cat]);
+                $article->addCategory($cat);
+            }
+
+            //Test l'article n'existe pas
+            if (!$this->articleRepository->findOneBy(["title" => $article->getTitle(), "content" => $article->getContent()])) {
+                $this->em->persist($article);
+                $this->em->flush();
+                $code = 201;
+            } else {
+                $code = 400;
+                $article = "Article existe déjà";
+            }
+        } else {
+            $code = 400;
+            $article = "Champs non remplis";
+        }
+
+        return $this->json(
+            $article,
+            $code,
+            [
+                "Access-Control-Allow-Origin" => "*",
+                "Content-Type" => "application/json"
+            ],
+            ['groups' => 'article:read']
+        );
+
     }
 }
